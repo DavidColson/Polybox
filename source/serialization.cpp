f568a44d0364f96b69f840542435829cb2c581b9
David Colson <davidcncolson@gmail.com>
2024-08-08 22:17:07 +0100

    Forget to stage the actual code from the last submit, actually robust table serialization this time lol

--- source/serialization.cpp
+++ source/serialization.cpp
@@ -7,50 +7,102 @@
 #include <string_builder.h>
 #include <light_string.h>
 #include <defer.h>
+#include <log.h>
 
 namespace Serialization {
 
-i32 Serialize(lua_State* L) {
-	// what did I recieve? If it's a table proceed with table iteration
+void SerializeRecursive(lua_State* L, i32 depth, StringBuilder& builder) {
+	// TODO:
+	// Need to error if we encounter a cyclic table reference
+	// need to deal with userdatas and other types
 
-    StringBuilder builder;
-
-    builder.Append("{\n");
-
-	if (lua_istable(L, 1)) {
+	if (lua_istable(L, -1)) {
 		lua_pushnil(L);
-		// stack now contains: -1 => nil; -2 => table
-		while (lua_next(L, -2))
-		{
-			// stack now contains: -1 => value; -2 => key; -3 => table
-			// copy the key so that lua_tostring does not modify the original
+		i32 arrayCounter = 0; // we'll set this to -1 if the sequence of indices breaks and we're in the hash part of the table
+		while (lua_next(L, -2)) {
 			lua_pushvalue(L, -2);
-			// stack now contains: -1 => key; -2 => value; -3 => key; -4 => table
-			const char *key = lua_tostring(L, -1);
-			if(lua_isnumber(L, -2)) {
-				f32 value = lua_tonumber(L, -2);
-				if (value == (i32)value) {
-					builder.AppendFormat("	\"%s\" = %i,\n", key, (i32)value);
+
+			// first decide if we're dealing with an array element or not
+			if (arrayCounter >= 0 && lua_isnumber(L, -1)) {
+				f32 key = lua_tonumber(L, -1);
+				if (key != (i32)key || (i32)key != ++arrayCounter) {
+					arrayCounter = -1;
+				}
+			} else {
+				arrayCounter = -1;
+			}
+
+			// array element
+			if (arrayCounter > 0) {
+				lua_pop(L, 1); // pop key copy
+				SerializeRecursive(L, 0, builder);
+			}
+			// dictionary element
+			else {
+				const char *key = lua_tostring(L, -1);
+				if (lua_isnumber(L, -1)) {
+					f32 key = lua_tonumber(L, -1);
+					if (key == (i32)key) {
+						builder.AppendFormat("[%i]=", (i32)key);
+					}
+					else {
+						builder.AppendFormat("[%f]=", key);
+					}
 				}
 				else {
-					builder.AppendFormat("	\"%s\" = %f,\n", key, value);
+					const char *key = lua_tostring(L, -1);
+					// if this string has an equal character in it, we must escape it
+					bool needsEscape = false;
+					char* c = (char*)key;
+					while (*c != 0) {
+						if (*c == '=')
+							needsEscape = true;
+						c++;
+					}
+					if (needsEscape)
+						builder.AppendFormat("[\"%s\"]=", key);
+					else
+						builder.AppendFormat("%s=", key);
 				}
-			}
-			else if(lua_isboolean(L, -2)) {
-				bool value = lua_toboolean(L, -2);
-				builder.AppendFormat("	\"%s\" = %s,\n", key, value ? "true" : "false");
-			}
-			else if (lua_isstring(L, -2)) {
-				const char *value = lua_tostring(L, -2);
-				builder.AppendFormat("	\"%s\" = \"%s\",\n", key, value);
-			}
-			// pop value + copy of key, leaving original key
-			lua_pop(L, 2);
-			// stack now contains: -1 => key; -2 => table
-		}	
-	}
 
-    builder.Append("}\n");
+				lua_pop(L, 1); // pop key copy
+				SerializeRecursive(L, 0, builder);
+			}
+
+			builder.AppendFormat(", ");
+			lua_pop(L, 1);
+		}
+		// remove the final ", "
+		builder.length -= 2;
+	}
+	if (lua_isnumber(L, -1)) {
+		f32 value = lua_tonumber(L, -1);
+		if (value == (i32)value) {
+			builder.AppendFormat("%i", (i32)value);
+		}
+		else {
+			builder.AppendFormat("%f", value);
+		}
+	}
+	else if(lua_isstring(L, -1)) {
+		const char *value = lua_tostring(L, -2);
+		builder.AppendFormat("\"%s\"", value);
+	}
+	else if (lua_isboolean(L, -1)) {
+		bool value = lua_toboolean(L, -1);
+		builder.AppendFormat("%s", value ? "true" : "false");
+	}
+	else if (lua_iscfunction(L, -1) || lua_isLfunction(L, -1)) {
+		luaL_error(L, "Cannot serialize functions");
+	}
+}
+
+i32 Serialize(lua_State* L) {
+    StringBuilder builder;
+
+    builder.Append("{");
+	SerializeRecursive(L, 0, builder);
+    builder.Append("}");
 
 	String result = builder.CreateString();
 	defer(FreeString(result));
